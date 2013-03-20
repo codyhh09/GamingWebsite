f9146ccde67fb79e687be8a909fa7991d85402ac
Carl Howard <choward8@ycp.edu>
2013-03-20 11:49:19 -0400

    something

---
+++ GamingWebsite/src/edu/ycp/cs320/gamingwebsite/shared/Memloc.java
@@ -0,0 +1,18 @@
+package edu.ycp.cs320.gamingwebsite.shared;
+
+public class Memloc {
+	private int x, y;
+	
+	public Memloc(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+}
