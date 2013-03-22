5a33396b5986b888ab0e97473edb303c049ab680
Carl Howard <choward8@ycp.edu>
2013-03-22 10:59:02 -0400

    yes

--- GamingWebsite/src/edu/ycp/cs320/gamingwebsite/shared/GamingWebUi.java
+++ GamingWebsite/src/edu/ycp/cs320/gamingwebsite/shared/GamingWebUi.java
@@ -11,42 +11,42 @@ import com.google.gwt.user.client.ui.RootPanel;
 
 
 
-public class GamingWebUi implements EntryPoint{
- static final String canvasHolderId = "canvasholder";
- static final String unsupportedBrowser = "Your browser does not support the HTML5 Canvas";
- static final int height = 400;
- static final int width = 500;
- final CssColor colorRed = CssColor.make("red");
- final CssColor colorGreen = CssColor.make("green");
- final CssColor colorBlue = CssColor.make("blue");
- Canvas canvas;
- Context2d context;
-
- public void onModuleLoad() {
-  canvas = Canvas.createIfSupported();
-  if (canvas == null) {
-	  	RootPanel.get(canvasHolderId).add(new Label(unsupportedBrowser));
-        return;
-  }
-  createCanvas();
- }
-
- private void createCanvas(){
-     canvas.setWidth(width + "px");
-     canvas.setHeight(height + "px");
-     canvas.setCoordinateSpaceWidth(width);
-	 canvas.setCoordinateSpaceHeight(height);
-     RootPanel.get(canvasHolderId).add(canvas);
-     context = canvas.getContext2d();
-     context.beginPath();
-     context.setFillStyle(colorRed);
-     context.fillRect(100, 50, 100, 100);
-     context.setFillStyle(colorGreen);
-     context.fillRect(200, 150, 100, 100);
-     context.setFillStyle(colorBlue);
-     context.fillRect(300, 250, 100, 100);
-     context.closePath();
- }
+//public class GamingWebUi implements EntryPoint{
+// static final String canvasHolderId = "canvasholder";
+// static final String unsupportedBrowser = "Your browser does not support the HTML5 Canvas";
+// static final int height = 400;
+// static final int width = 500;
+// final CssColor colorRed = CssColor.make("red");
+// final CssColor colorGreen = CssColor.make("green");
+// final CssColor colorBlue = CssColor.make("blue");
+// Canvas canvas;
+// Context2d context;
+//
+// public void onModuleLoad() {
+//  canvas = Canvas.createIfSupported();
+//  if (canvas == null) {
+//	  	RootPanel.get(canvasHolderId).add(new Label(unsupportedBrowser));
+//        return;
+//  }
+//  createCanvas();
+// }
+//
+// private void createCanvas(){
+//     canvas.setWidth(width + "px");
+//     canvas.setHeight(height + "px");
+//     canvas.setCoordinateSpaceWidth(width);
+//	 canvas.setCoordinateSpaceHeight(height);
+//     RootPanel.get(canvasHolderId).add(canvas);
+//     context = canvas.getContext2d();
+//     context.beginPath();
+//     context.setFillStyle(colorRed);
+//     context.fillRect(100, 50, 100, 100);
+//     context.setFillStyle(colorGreen);
+//     context.fillRect(200, 150, 100, 100);
+//     context.setFillStyle(colorBlue);
+//     context.fillRect(300, 250, 100, 100);
+//     context.closePath();
+// }
 
 }
 
